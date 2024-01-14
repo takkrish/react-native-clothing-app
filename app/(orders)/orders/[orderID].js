f964adda1cb4f4b4ea5f0e3b128e579b38688dfe
Krish Tak <takrishtak2002@gmail.com>
2024-01-14 22:38:44 +0530

    Creating OrderId page for particular order

--- app/(orders)/orders/[orderID].js
+++ app/(orders)/orders/[orderID].js
@@ -1,13 +1,34 @@
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import React from 'react';
-import { useLocalSearchParams } from 'expo-router';
+import { router, useLocalSearchParams } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const OrderID = () => {
 	const { orderID } = useLocalSearchParams();
 	return (
 		<SafeAreaView style={{ flex: 1 }}>
-			<Text>{orderID}</Text>
+			<View className='flex flex-row justify-between items-center bg-white py-2'>
+				<TouchableOpacity
+					className='bg-zinc-100 rounded-full w-12 h-12 flex justify-center items-center'
+					onPress={() =>
+						router.canGoBack()
+							? router.back()
+							: router.replace('(dashboard)/account')
+					}>
+					<Icon name='arrow-back' size={24} />
+				</TouchableOpacity>
+				<Text
+					className='text-xs'
+					style={{
+						fontFamily: 'Inter_500Medium',
+					}}>
+					{orderID}
+				</Text>
+				<TouchableOpacity className='bg-zinc-100 rounded-full w-12 h-12 flex justify-center items-center'>
+					<Icon name='information-circle' size={30} />
+				</TouchableOpacity>
+			</View>
 		</SafeAreaView>
 	);
 };
